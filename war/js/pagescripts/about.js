0a9da04be979d66fee320b98ccf7e7357160a829
Sushain Cherivirala <sushain97@gmail.com>
2013-11-17 15:20:11 -0600

    Migrate About Page Modal to Bootstrap 3

--- war/js/pagescripts/about.js
+++ war/js/pagescripts/about.js
@@ -18,9 +18,7 @@
 $(document).ready(function() {
 	$('h1 button').on('click', function() {
 		$('.accordion-body').addClass('in');
-		$('button').hide();
 		window.print();
 		$('.accordion-body').removeClass('in');
-		$('button').show();
 	});
 });

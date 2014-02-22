1640c8e98955372efe782ad7824144828899f4ee
Sushain Cherivirala <sushain97@gmail.com>
2014-02-21 22:51:56 -0600

    Add Custom Error Page for HTTP Error 400

---
+++ src/errors/BadRequest_400.java
@@ -0,0 +1,41 @@
+/*
+ * Component of GAE Project for TMSCA Contest Automation
+ * Copyright (C) 2013 Sushain Cherivirala
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see [http://www.gnu.org/licenses/].
+ */
+
+package errors;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+
+@SuppressWarnings("serial")
+public class BadRequest_400 extends ErrorServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		VelocityEngine ve = new VelocityEngine();
+		ve.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, "html/errors, html/snippets");
+		ve.init();
+		VelocityContext context = new VelocityContext();
+		init(context, req);
+		close(context, ve.getTemplate("error400.html"), resp);
+	}
+}

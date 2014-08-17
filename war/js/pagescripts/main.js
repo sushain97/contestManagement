15cfcc1b2aa526f138e1760f3f8407d6aa23fe7b
Sushain Cherivirala <sushain97@gmail.com>
2014-08-17 13:36:44 -0500

    Allow user to import registration data

--- war/js/pagescripts/main.js
+++ war/js/pagescripts/main.js
@@ -84,6 +84,51 @@ $(document).ready(function() {
 		$('input[name=studentData]').remove();
 		$(this).append($('<input>').attr('type', 'hidden').attr('name', 'studentData').val(JSON.stringify(students)));
 	});
+
+	$('button#import').click(function() {
+		var students = $('textarea#importData').val().split('\n');
+		var seperator = $('input#seperator').val();
+
+		$.each(students, function() {
+			var fields = this.split(seperator);
+			if(fields.length == 6 && parseInt(fields[1].trim()))
+				addStudent(fields[0].trim(), fields[1].trim(), [!!fields[2].trim().length, !!fields[3].trim().length, !!fields[4].trim().length, !!fields[5].trim().length]);
+		});
+	});
+
+	var refreshTable = function() {
+		var students = $('textarea#importData').val().split('\n');
+		var seperator = $('input#seperator').val();
+
+		var tableBody = $('table#importTable tbody');
+		tableBody.empty();
+
+		$.each(students, function() {
+			var fields = this.split(seperator);
+			var tr = $('<tr></tr>');
+
+			if(fields.length < 6 || !parseInt(fields[1].trim())) {
+				tr.addClass("danger");
+				tr.append($('<td></td>').text('Failed'));
+			}
+			else {
+				tr.append($('<td></td>').text(fields[0].trim()));
+				tr.append($('<td></td>').text(fields[1].trim()));
+
+
+				var checked = '<i class="glyphicon glyphicon-ok"></i>', unchecked = '<i class="glyphicon glyphicon-remove"></i>';
+				tr.append($('<td></td>').html(fields[2].trim().length ? checked : unchecked).addClass(fields[2].trim().length ? "success" : "danger"));
+				tr.append($('<td></td>').html(fields[3].trim().length ? checked : unchecked).addClass(fields[3].trim().length ? "success" : "danger"));
+				tr.append($('<td></td>').html(fields[4].trim().length ? checked : unchecked).addClass(fields[4].trim().length ? "success" : "danger"));
+				tr.append($('<td></td>').html(fields[5].trim().length ? checked : unchecked).addClass(fields[5].trim().length ? "success" : "danger"));
+			}
+
+			tableBody.append(tr);
+		});
+	};
+
+	$('textarea#importData').on('propertychange keyup input paste', refreshTable);
+	$('input#seperator').on('input', refreshTable);
 });
 
 $(window).load(function() {
@@ -117,6 +162,28 @@ function addStudent(name, grade, subjects) {
 	$('#registrations tbody').append(tr);
 	$('#registrations').trigger('update');
 	CalcCost();
+}
+
+function addStudentRow(name, grade, subjects) {
+	var tr = $('<tr class="student"></tr>');
+
+	tr.append($('<td class="text-center"></td>').text(name));
+	tr.append($('<td class="text-center"></td>').text(grade));
+
+	for(var j = 0; j < 4; j++) {
+		var td = $('<td class="text-center"></td>');
+		if(subjects[j]) {
+			td.html('<span class="hide">1</span><i class="glyphicon glyphicon-ok"></i>');
+			td.addClass('success');
+		}
+		else {
+			td.html('<span class="hide">0</span><i class="glyphicon glyphicon-remove"></i>');
+			td.addClass('danger');
+		}
+		tr.append(td);
+	}
+
+	$('#registrations tbody').append(tr);
 }
 
 function CalcCost() {

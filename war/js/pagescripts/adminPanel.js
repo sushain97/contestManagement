cfeb2cfcd4b994edbbf49c9cb4945b1a25ec634f
Sushain Cherivirala <sushain@skc.name>
2015-02-06 17:41:20 -0600

    Add sections to Admin Panel (fixes #62)

--- war/js/pagescripts/adminPanel.js
+++ war/js/pagescripts/adminPanel.js
@@ -16,6 +16,8 @@
  */
 
 $(document).ready(function() {
+	$('.nav-pills').stickyTabs();
+
 	CheckPassChange();
 	CheckUpdate();
 
@@ -124,3 +126,36 @@ function signInCallback(authResult) {
 		$('#oAuthResult').html('<strong class="text-danger">Failed to sign you in, try again.</strong>');
 	}
 }
+
+/**
+ * jQuery Plugin: Sticky Tabs
+ *
+ * @author Aidan Lister <aidan@php.net>
+ * @version 1.0.0
+ */
+
+(function ( $ ) {
+	$.fn.stickyTabs = function() {
+		context = this
+
+		// Show the tab corresponding with the hash in the URL, or the first tab.
+		var showTabFromHash = function() {
+			var hash = window.location.hash;
+			var selector = hash ? 'a[href="' + hash + '"]' : 'li:first-child a';
+			$(selector, context).tab('show');
+		}
+
+		// Set the correct tab when the page loads
+		showTabFromHash(context)
+
+		// Set the correct tab when a user uses their back/forward button
+		window.addEventListener('hashchange', showTabFromHash, false);
+
+		// Change the URL when tabs are clicked
+		$('a', context).on('click', function(e) {
+			history.pushState(null, null, this.href);
+		});
+
+		return this;
+	};
+}( jQuery ));
